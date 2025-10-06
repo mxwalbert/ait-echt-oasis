9d2ecca1d791e0746994cec07b2d9c8cb9fc4101
mxwalbert <wolf.maxi@gmail.com>
2025-10-06 13:57:51 +0200

    Added nomad-ait-echt-oasis plugin

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+uv pip install --upgrade 'nomad-ait-echt-oasis @ git+https://github.com/mxwalbert/nomad-ait-echt-oasis.git'
+
 python -m nomad.cli admin run app --with-gui --gunicorn --host 0.0.0.0 $@
